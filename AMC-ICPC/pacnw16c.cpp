1e917c5aa2d582c6716148d8c406233154bcf3d5
Carson <31937503+Carson-Tang@users.noreply.github.com>
2019-03-29 19:48:10 -0400

    Create pacnw16c.cpp

---
+++ AMC-ICPC/pacnw16c.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int cnt,n,k,r,v,ans;
+bool c[100001];
+int main(){
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>k>>r;
+    for(int i = 0; i < k; i++){
+        cin>>v;
+        c[v]=1;
+    }
+    for(int i = 0; i < r; i++) if(c[i]) cnt++;
+    int lb = 0, rb = r-1;
+    while(rb<n){
+        int idx = rb;
+        while(cnt<2){
+            if(!c[idx]) {
+                c[idx]=1;
+                idx--;
+                cnt++;
+                ans++;
+            }
+        }
+        cnt-=c[lb++];
+        cnt+=c[++rb];
+
+    }
+    cout<<ans<<"\n";
+    return 0;
+}
