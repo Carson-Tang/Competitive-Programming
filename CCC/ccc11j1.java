3e374d15097afd2bb13e2cba195e34ff2223efb5
Carson <31937503+Carson-Tang@users.noreply.github.com>
2019-08-09 16:10:26 -0400

    Create ccc11j1.java

---
+++ CCC/ccc11j1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int e = sc.nextInt();
+        if(a >= 3 && e <= 4) {
+            System.out.println("TroyMartian");
+        }
+        if(a <= 6 && e >= 2){
+            System.out.println("VladSaturnian"); 
+        }
+        if(a <= 2 && e <= 3){
+            System.out.println("GraemeMercurian");
+        }
+    }
+}
